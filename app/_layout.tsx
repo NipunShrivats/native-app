24eb81c03ba862fb9537c14eb7026cfb4498c3dd
Nipun Shrivats <nipunrawat16@gmail.com>
2026-06-07 22:03:38 +0530

    latest

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
 
+  return (
     <Stack>
       {/* <Stack.Screen name="index" />       */}
       {/* <Stack.Screen name="about/about" /> */}
@@ -14,7 +14,6 @@ export default function RootLayout() {
       <Stack.Screen name="signup" options={{ headerShown: false }} />
       <Stack.Screen name="loginSubmit" options={{ headerShown: false }} />
       <Stack.Screen name="regsiterSubmit" options={{ headerShown: false }} />
-
     </Stack>
 
   )
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,57 @@
 import MyButton from "@/components/MyButton";
+import { BlurView } from 'expo-blur';
 import { useRouter } from "expo-router";
-import { Image, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
-export default function Index() {
 
+import { Poppins_600SemiBold_Italic as pbi, Poppins_400Regular_Italic as pri, useFonts } from "@expo-google-fonts/poppins";
+
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+SplashScreen.preventAutoHideAsync();
+export default function Index() {
   const router = useRouter();
   const onContinue = () => {
     router.navigate("/signup");
-  }
+  };
+
+  const [loaded, error] = useFonts({
+    pri,
+    pbi
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Image source={require('@/assets/images/homepage.png')} className="w-full h-full absolute" resizeMode="cover" />
-      <View>
-        <MyButton btnName={"Continue"} onPress={onContinue} />
-      </View>
+      <Image
+        source={require("@/assets/images/homepage.png")}
+        className="w-full h-full absolute"
+        resizeMode="cover"
+      />
+      <BlurView className="flex-1 w-full h-full items-center justify-center"
+        tint="dark">
+        <View className="flex items-center">
+          <MyButton btnName={"Continue"} onPress={onContinue} />
+          <Text className="text-orange-600" style={{
+            fontFamily: "pbi",
+            fontSize: 20
+          }}>
+            Welcome Sir
+          </Text>
+        </View>
+
+        <Text>
+          Hello
+        </Text>
+      </BlurView>
     </SafeAreaView>
-  )
+  );
 }
